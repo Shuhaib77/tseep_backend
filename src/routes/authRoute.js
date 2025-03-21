3b89e354f0564f96489fe182c8543ee520f06a6a
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-03-21 14:17:26 +0530

    updated

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { login, register } from "../controller/auth.js";
+import tryCatch from "../middleware/tryCatch.js";
+import { errorHndler } from "../middleware/globelError.js";
 
-const authRoute=express.Router()
+const authRoute = express.Router();
 
+authRoute.post("/register", errorHndler, tryCatch(register));
+authRoute.post("/login", errorHndler, tryCatch(login));
 
-authRoute.post("/register",register)
-authRoute.post("/login",login)
-
-export default authRoute
+export default authRoute;
