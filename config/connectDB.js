12490a3c092263683d52e8a430ffb32e11930b5b
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-03-21 12:03:24 +0530

    updated

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,17 +1,13 @@
 import mongoose from "mongoose";
-
-export const DB=()=>{
-    mongoose
-  .connect(
-    "mongodb+srv://shuhaibbasheer20022:lL5lLuVCb8PIjgD7@cluster0.e4jd9.mongodb.net/"
-  )
-  .then(() => {
-    console.log("database connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-}
-
-
+import dotenv from "dotenv";
+dotenv.config();
+export const DB = () => {
+  mongoose
+    .connect(process.env.MONGODB_CLIENT)
+    .then(() => {
+      console.log("database connected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
